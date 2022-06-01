5bc85b2fb12866a9f9b395d15e6673ad3e2cc963
Paulo Tosi <tosi.paulo@gmail.com>
2022-06-01 14:25:54 -0300

    Criando voltar para container

--- projects/ui/src/lib/components/container/container.module.ts
+++ projects/ui/src/lib/components/container/container.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './container.component';
@@ -9,7 +10,8 @@ import { ContainerComponent } from './container.component';
     ContainerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     ContainerComponent
