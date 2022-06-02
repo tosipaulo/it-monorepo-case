19c931dfec205b70c3e82693c35b8562fdc61fab
Paulo Tosi <tosi.paulo@gmail.com>
2022-06-01 22:53:16 -0300

    Criando pagina de cadastro de gasto

--- projects/ui/src/lib/components/container/container.component.ts
+++ projects/ui/src/lib/components/container/container.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ContainerComponent implements OnInit {
 
   @Input() hasMenuToBack: boolean = true;
+  @Input() actions: boolean = false;
 
   constructor() { }
 
